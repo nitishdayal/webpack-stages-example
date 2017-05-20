01317588e827fbcc503aac839c561da84b3d7b52
Nitish Dayal <nitishdayal@outlook.com>
2017-05-20 14:09:54 -0700

    Add comments, NPM scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
-module.exports = env => ({
-  devtool: env === "production" ? "source-map" : "cheap-eval-source-map",
-  entry: "./src/index.js",
-  output: {
-    filename: "./build/bundle.js"
-  },
-  resolve: {
-    extensions: [".js"]
-  }
-});
+module.exports = env => {
+  const devtool = env === "production" ? "source-map" : "cheap-eval-source-map";
+
+  /**
+   * ES5 Equivalent:
+   *
+   * var devtool;
+   * if (env === 'production') {
+   *     devtool = 'source-map'
+   * } else {
+   *     devtool = 'cheap-eval-source-map'
+   * }
+   */
+
+  return {
+    devtool,
+    entry: "./src/index.js",
+    output: {
+      filename: "./build/bundle.js"
+    },
+    resolve: {
+      extensions: [".js"]
+    }
+  };
+};
