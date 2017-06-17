2a5cae86257197068250bc81fa56c9c373a33a71
Nitish Dayal <nitishdayal@outlook.com>
2017-06-17 09:56:09 -0700

    Minor fixes, formatting, update webpack version

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Hello from "./app/sayHello";
 // and refer to it in this file as 'Hello'
 
 const name = "Nitish";
+
 // Reference to the <div id="root"> element in
 const root = document.getElementById("root");
 
